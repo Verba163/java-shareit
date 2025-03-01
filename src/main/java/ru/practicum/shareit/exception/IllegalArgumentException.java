12973e3e2dad624b167cdaa8973fab93eea8ee3f
DmitriiFomin <dmi7f0min@yandex.ru>
2025-03-01 17:11:31 +0300

    1.Исправить чекстайл

--- src/main/java/ru/practicum/shareit/exception/IllegalArgumentException.java
+++ src/main/java/ru/practicum/shareit/exception/IllegalArgumentException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exception;
 
-public class IllegalArgumentException extends RuntimeException{
+public class IllegalArgumentException extends RuntimeException {
     public IllegalArgumentException(String message) {
         super(message);
     }
